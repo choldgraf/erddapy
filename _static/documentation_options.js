871d1ae77d718f9acdea3958113f3618493cb669
Travis CI User <travis@example.org>
2018-05-29 17:49:55 +0000

    Update docs after building Travis build 98 of pyoceans/erddapy
    The docs were built from the branch 'master' against the commit
    b72520304c887f3604d2b4d90d5bed4232333eab.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/pyoceans/erddapy/jobs/385316219.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.6/site-packages/doctr/__main__.py deploy --sync .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.1.7+7.g4a6759c',
+    VERSION: '0.1.7+8.gb725203',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
